a8ad9e6c5cfcf3c75d3c4ec0c06716274b6bebe1
Prabhat Singh <prabhatsingh5552>
2023-11-25 17:29:50 +0530

    Frist Commit

--- JavaProject1/src/JavaClass1.java
+++ JavaProject1/src/JavaClass1.java
@@ -2,33 +2,6 @@
 public class JavaClass1 {
 
 	public static void main(String[] args) {
-		//initial commit
-		//initial commit
-		//First First commit
-		//Second Secdon commit
-		//Second Secdon commit
-		//Second Secdon commit rebase
-		//Second Secdon commit master
-		//Second Secdon commit rebase 1
-		// git merge 1 
-		// git merge 2 
-		// git rebase 1 
-		// git rebase 2 
-		//Second Secdon commit master
-		
-		
-		// git rebase 11 
-		// git rebase 12
-		
-		// git rebase 13
-		// git rebase 14
-		
-		//first megre from branch2
-		
-		//frist commit from branch3
-		//second commit from branch3
-	
-		//First commit from master
-		//Second commit from master
+		//FirstCommit
 	}
 }
